7cd44c0e852e23e40e11f60608253e4fd54eaaae
Matheo Marmounier <matheo.marmounier@gmail.com>
2024-06-08 12:56:48 +0200

    Create test_connexion.php

---
+++ test_connexion.php
@@ -0,0 +1,17 @@
+<?php
+// Inclure le fichier des paramètres de connexion à la base de données
+include('database.php');
+
+// Essayer de se connecter à la base de données
+$connexion = new mysqli($serveur, $utilisateur, $motDePasse, $baseDeDonnees);
+
+// Vérifier la connexion
+if ($connexion->connect_error) {
+    die("La connexion à la base de données a échoué : " . $connexion->connect_error);
+} else {
+    echo "Connexion à la base de données réussie !";
+}
+
+// Fermer la connexion à la base de données
+$connexion->close();
+?>
