5a6bd2c3419c102d58d8de93ab2c520e1c171778
Matheo Marmounier <matheo.marmounier@gmail.com>
2024-06-08 12:49:43 +0200

    Update connexion.php

--- connexion.php
+++ connexion.php
@@ -71,7 +71,7 @@ if (!isset($_SESSION['username'])) {
               <?php if (!empty($error_message)): ?>
                   <p class="text-red-500"><?php echo $error_message; ?></p>
               <?php endif; ?>
-              <form class="space-y-4 md:space-y-6" action="login.php" method="post">
+              <form class="space-y-4 md:space-y-6" action="connexion.php" method="post">
                   <div>
                       <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Votre Email</label>
                       <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="nom@societe.com" required="">
