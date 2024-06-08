72e771119b0780250d3aeea819c8ffcb4ab25c07
Matheo Marmounier <matheo.marmounier@gmail.com>
2024-06-08 11:59:53 +0200

    f

--- database.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Paramètres de connexion à la base de données
-$serveur = "192.168.1.24";
-$utilisateur = "nouveau_utilisateur";
-$motDePasse = "mot_de_passe";
-$baseDeDonnees = "dashboard";
-?>
