abe44cd8efc0d5360cf845b5c24901b67bf8cc05
Matheo Marmounier <matheo.marmounier@gmail.com>
2024-06-09 18:57:28 +0200

    Update test_connexion.php

--- test_connexion.php
+++ test_connexion.php
@@ -3,7 +3,10 @@ error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
 // Inclure le fichier des paramètres de connexion à la base de données
-include('database.php');
+$serveur = "192.168.1.24";
+$utilisateur = "pma-admin";
+$motDePasse = "MotdePasseComplexe2";
+$baseDeDonnees = "dashboard";
 
 // Essayer de se connecter à la base de données
 $connexion = new mysqli($serveur, $utilisateur, $motDePasse, $baseDeDonnees);
