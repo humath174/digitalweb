9a1914da1ecfa00a6695771bb29a44a06dcb0070
Matheo Marmounier <matheo.marmounier@gmail.com>
2024-06-09 19:39:46 +0200

    nettoiage dossier

--- component/sidebar.php
+++ component/sidebar.php
@@ -1,35 +1,114 @@
-<?php  session_start(); ?>
-<div class="bg-sidebar vh-100 w-50 position-fixed">
-            <div class="log d-flex justify-content-between">
-                <h1 class="E-classe text-start ms-3 ps-1 mt-3 h6 fw-bold">DigitalGroup</h1>
-                <i class="far fa-times h4 me-3 close align-self-end d-md-none"></i>
+<div class="container flex flex-col mx-auto bg-white">
+            <aside class="group/sidebar flex flex-col shrink-0 lg:w-[300px] w-[250px] transition-all duration-300 ease-in-out m-0 fixed z-40 inset-y-0 left-0 bg-white border-r border-r-dashed border-r-neutral-200 sidenav fixed-start loopple-fixed-start" id="sidenav-main"><div class="flex shrink-0 px-8 items-center justify-between h-[96px]">
+            <a class="transition-colors duration-200 ease-in-out" href="https://www.loopple.com">
+            <img alt="Logo" src="https://raw.githubusercontent.com/Loopple/loopple-public-assets/main/riva-dashboard-tailwind/img/logos/loopple.svg" class="inline">
+            </a>
+        </div>
+
+        <div class="hidden border-b border-dashed lg:block dark:border-neutral-700/70 border-neutral-200"></div>
+
+        <div class="flex items-center justify-between px-8 py-5">
+            <div class="flex items-center mr-5">
+            <div class="mr-5">
+                <div class="inline-block relative shrink-0 cursor-pointer rounded-[.95rem]">
+           
+                </div>
             </div>
-            <div class="img-admin d-flex flex-column align-items-center text-center gap-2">
-                <img class="rounded-circle" src="/bbra/component/img-admin.jpeg" alt="img-admin" height="120" width="120">
-                <h2 class="h6 fw-bold"><?php echo $_SESSION['username']; ?></h2>
-                <span class="h7 admin-color">Dashboard</span>
+            <div class="mr-2 ">
+                <a href="javascript:void(0)" class="dark:hover:text-primary hover:text-primary transition-colors duration-200 ease-in-out text-[1.075rem] font-medium dark:text-neutral-400/90 text-secondary-inverse">Robert Jason</a>
+                <span class="text-secondary-dark dark:text-stone-500 font-medium block text-[0.85rem]">SEO Manager</span>
             </div>
-            <div class=" bg-list d-flex flex-column align-items-center fw-bold gap-2 mt-4 ">
-                <ul class="d-flex flex-column list-unstyled">
-                        <li class="h7"><a class="nav-link text-dark" href="/bbra/welcome.php"><i
-                            class="fal fa-home-lg-alt me-2"></i> <span>Acceuil</span></a></li>
-                    <li class="h7"><a class=" nav-link text-dark" href="/bbra/contact-rec.php"><i
-                                class="fal fa-bookmark me-2"></i> <span>Contact</span></a></li>
-                    <li class="h7"><a class=" nav-link text-dark" href="/bbra/devis-rec.php"><i
-                                class="far fa-graduation-cap me-2"></i> <span>Devis</span></a></li>
-                    <li class="h7"><a class=" nav-link text-dark" href="/bbra/dashboard-img.php"><i
-                                class="fal fa-usd-square me-2"></i> <span>Photo</span></a></li>
-                    <li class="h7"><a class=" nav-link text-dark" href="#"><i
-                                class="fal fa-file-chart-line me-2"></i> <span>Réalisation</span></a></li>
-                    <li class="h7"><a class=" nav-link text-dark" href="/bbra/fichier_client.php"><i
-                                    class="fal fa-bookmark me-2"></i> <span>Fichier client</span></a></li>
-                    <li class="h7"><a class=" nav-link text-dark" href=""><i
-                                class="fal fa-sliders-v-square me-2"></i> <span>Garder</span></a></li>
-                </ul>
-                <ul class="logout d-flex justify-content-start list-unstyled">
-                    <li class=" h7"><a class="nav-link text-dark" href="logout.php"><span>Logout</span><i
-                                class="fal fa-sign-out-alt ms-2"></i></a></li>
-                </ul>
+            </div>
+            <a class="inline-flex relative items-center group justify-end text-base font-medium leading-normal text-center align-middle cursor-pointer rounded-[.95rem] transition-colors duration-150 ease-in-out text-dark bg-transparent shadow-none border-0" href="javascript:void(0)">
+            <span class="leading-none transition-colors duration-200 ease-in-out peer shrink-0 group-hover:text-primary text-secondary-dark">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z"></path>
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
+                </svg>
+            </span>
+            </a>
+        </div>
+
+        <div class="hidden border-b border-dashed lg:block dark:border-neutral-700/70 border-neutral-200"></div>
+
+        <div class="relative pl-3 my-5 overflow-y-scroll">
+            <div class="flex flex-col w-full font-medium">
+
+            <!-- Acceuil -->
+            <div class="block pt-5 pb-[.15rem]">
+                <div class="px-4 py-[.65rem]">
+                <span class="font-semibold text-[0.95rem] uppercase dark:text-neutral-500/80 text-secondary-dark">Principal</span>
+                </div>
             </div>
 
+            <div>
+                <span class="select-none flex items-center px-4 py-[.775rem] cursor-pointer my-[.4rem] rounded-[.95rem]">
+                <a href="javascript:;" class="flex items-center flex-grow text-[1.15rem] dark:text-neutral-400/75 text-stone-500 hover:text-dark">Acceuil</a>
+                </span>
+            </div>
+
+            <div>
+                <span class="select-none flex items-center px-4 py-[.775rem] cursor-pointer my-[.4rem] rounded-[.95rem]">
+                <a href="javascript:;" class="flex items-center flex-grow text-[1.15rem] dark:text-neutral-400/75 text-stone-500 hover:text-dark">Entreprise</a>
+                </span>
+            </div>
+
+             <!-- Contact -->
+            <div class="block pt-5 pb-[.15rem]">
+                <div class="px-4 py-[.65rem]">
+                <span class="font-semibold text-[0.95rem] uppercase dark:text-neutral-500/80 text-secondary-dark">Formulaire Site Web</span>
+                </div>
+            </div>
+
+            <div>
+                <span class="select-none flex items-center px-4 py-[.775rem] cursor-pointer my-[.4rem] rounded-[.95rem]">
+                <a href="javascript:;" class="flex items-center flex-grow text-[1.15rem] dark:text-neutral-400/75 text-stone-500 hover:text-dark">Contact</a>
+                </span>
+            </div>
+
+            <div>
+                <span class="select-none flex items-center px-4 py-[.775rem] cursor-pointer my-[.4rem] rounded-[.95rem]">
+                <a href="javascript:;" class="flex items-center flex-grow text-[1.15rem] dark:text-neutral-400/75 text-stone-500 hover:text-dark">Devis</a>
+                </span>
+            </div>
+
+
+            <!-- Facturation -->
+            <div class="block pt-5 pb-[.15rem]">
+                <div class="px-4 py-[.65rem]">
+                <span class="font-semibold text-[0.95rem] uppercase dark:text-neutral-500/80 text-secondary-dark">Facturation</span>
+                </div>
+            </div>
+
+
+            <div>
+                <span class="select-none flex items-center px-4 py-[.775rem] cursor-pointer my-[.4rem] rounded-[.95rem]">
+                <a href="javascript:;" class="flex items-center flex-grow text-[1.15rem] dark:text-neutral-400/75 text-stone-500 hover:text-dark">Facture</a>
+                </span>
+            </div>
+
+
+             <!-- Dossier client -->
+
+            <div class="block pt-5 pb-[.15rem]">
+                <div class="px-4 py-[.65rem]">
+                <span class="font-semibold text-[0.95rem] uppercase dark:text-neutral-500/80 text-secondary-dark">Client</span>
+                </div>
+            </div>
+
+            <div>
+                <span class="select-none flex items-center px-4 py-[.775rem] cursor-pointer my-[.4rem] rounded-[.95rem]">
+                <a href="javascript:;" class="flex items-center flex-grow text-[1.15rem] dark:text-neutral-400/75 text-stone-500 hover:text-dark">Dossier Client</a>
+                </span>
+            </div>
+
+
+            
+
+            
+
+            </div>
+            </div>
+        </aside>
         </div>
+        
