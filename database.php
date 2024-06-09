827b660cd5aa85e09358e78359c1736c6cbca54a
Matheo Marmounier <matheo.marmounier@gmail.com>
2024-06-09 13:25:29 +0200

    m

--- database.php
+++ database.php
@@ -1,7 +1,6 @@
 <?php
-// Paramètres de connexion à la base de données
 $serveur = "192.168.1.24";
-$utilisateur = "nouveau_utilisateur";
+$utilisateur = "nouvel_utilisateur";
 $motDePasse = "mot_de_passe";
 $baseDeDonnees = "dashboard";
 ?>
--- test_connexion.php
+++ test_connexion.php
@@ -1,4 +1,7 @@
 <?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
 // Inclure le fichier des paramètres de connexion à la base de données
 include('database.php');
 
