8b8cde0eef656151a0176bf9c7d2377340367939
Matheo Marmounier <matheo.marmounier@gmail.com>
2024-06-09 19:12:16 +0200

    Update login.php

--- login.php
+++ login.php
@@ -6,46 +6,43 @@ session_start();
 
 if (!isset($_SESSION['username'])) {
 
-// Vérifier la connexion à la base de données
-include('database.php');
+// Paramètres de connexion à la base de données
+    include('database.php');
 
 // Créer une connexion à la base de données
-$connexion = new mysqli($serveur, $utilisateur, $motDePasse, $baseDeDonnees);
+    $connexion = new mysqli($serveur, $utilisateur, $motDePasse, $baseDeDonnees);
 
 // Vérifier la connexion
-if ($connexion->connect_error) {
-    die("La connexion à la base de données a échoué : " . $connexion->connect_error);
-}
+    if ($connexion->connect_error) {
+        die("La connexion à la base de données a échoué : " . $connexion->connect_error);
+    }
 
-// Récupérer les données du formulaire et échapper les caractères spéciaux pour éviter les injections SQL
-$username = $connexion->real_escape_string($_POST['email']);
-$password = $connexion->real_escape_string($_POST['password']);
+// Récupérer les données du formulaire
+    $username = $_POST['email'];
+    $password = $_POST['password'];
 
-// Utiliser les requêtes préparées pour éviter les injections SQL
-$requete = $connexion->prepare("SELECT * FROM Users WHERE username = ? AND password_hash = ?");
-$requete->bind_param("ss", $username, $password);
-$requete->execute();
-$resultat = $requete->get_result();
+// Vérifier l'authenticité de l'utilisateur
+// Note : Ceci est un exemple très basique et ne doit pas être utilisé en production
+    $requete = "SELECT * FROM Users WHERE email = '$username' AND password_hash = '$password'";
+    $resultat = $connexion->query($requete);
 
-if ($resultat->num_rows > 0) {
-    // Connexion réussie, enregistrer l'identifiant de l'utilisateur dans la session
-    $_SESSION['username'] = $username;
-    echo "Connexion réussie ! Bienvenue, $username.";
+    if ($resultat->num_rows > 0) {
+        // Connexion réussie, enregistrer l'identifiant de l'utilisateur dans la session
+        $_SESSION['username'] = $username;
+        echo "Connexion réussie ! Bienvenue, $username.";
 
-    // Rediriger vers une page sécurisée par exemple
-    header("Location: welcome.php");
-    exit();
-} else {
-    echo "Nom d'utilisateur ou mot de passe incorrect.";
-}
+        // Rediriger vers une page sécurisée par exemple
+        header("Location: index.php");
+    } else {
+        echo "Nom d'utilisateur ou mot de passe incorrect.";
+    }
 
 // Fermer la connexion à la base de données
-$requete->close();
-$connexion->close();
+    $connexion->close();
 
 } else {
     // Afficher un message personnalisé si la session est active
-    echo "<a href='welcome.php'>Tableaux de bord</a>";
+    echo "<a href='index.php'>Tableaux de bord</a>";
 }
 
 ?>
